521db9a98379e1f82dc44e88f2b10ecb32b165b4
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-06-07 17:43:33 +0600

    Installing Laravel socialite package and Update Services.php file

--- config/services.php
+++ config/services.php
@@ -32,17 +32,16 @@ return [
 
 
     // for facebook
-    'github' => [
-        'client_id' => env('GITHUB_CLIENT_ID'),
-        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => 'http://example.com/callback-url',
     ],
 
-
     // for github
-    'github' => [
-        'client_id' => env('GITHUB_CLIENT_ID'),
-        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => 'http://example.com/callback-url',
     ],
 
